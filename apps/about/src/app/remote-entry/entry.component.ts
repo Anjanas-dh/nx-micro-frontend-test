f7b4f9e69f482630fe80e65b5f997995afdd9c52
Anja van Hagen <anja.vanhagen@ns.nl>
2022-05-18 10:32:05 +0200

    Set up micro frontend host and remotes via https://nx.dev/module-federation/faster-builds

---
+++ apps/about/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about-entry',
+  template: `<mftest-nx-welcome></mftest-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
---
+++ apps/cart/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cart-entry',
+  template: `<mftest-nx-welcome></mftest-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
